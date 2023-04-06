96c4176119a40e623cdac723ee800e5d59459d54
wrathboyx <stevenjhnny90@gmail.com>
2023-04-06 11:40:53 +0700

    menampilkan semua data mahasiwa di tabel database

--- controller.js
+++ controller.js
@@ -6,3 +6,14 @@ var connection = require("./connect");
 exports.index = function (req, res) {
   response.ok("Applikasi Rest API ku berjalan", res);
 };
+
+// Menampilkan semua data mahasiswa
+exports.tampilkan = function (req, res) {
+  connection.query("SELECT * FROM mahasiswa", function (error, rows, fields) {
+    if (error) {
+      connection.log(error);
+    } else {
+      response.ok(rows, res);
+    }
+  });
+};
