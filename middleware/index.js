ece441befc8d741982e48e30bc6b251c1d0d92cb
wrathboyx <stevenjhnny90@gmail.com>
2023-04-12 13:19:18 +0700

    login user

--- middleware/index.js
+++ middleware/index.js
@@ -4,5 +4,6 @@ var router = express.Router();
 
 // Daftarkan menu regristrasi
 router.post("/api/v1/register", auth.register);
+router.post("/api/v1/login", auth.login);
 
 module.exports = router;
